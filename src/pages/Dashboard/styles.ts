701b90253e70e88d1399c123175f4799f97f5319
Cristian Butzke <cristian.btzk@gmail.com>
2020-09-08 11:14:07 -0300

    repository details

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -61,7 +65,7 @@ export const Repositories = styled.div`
   }
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -72,6 +76,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+        border-right: 0;
+      `}
 
     &:placeholder {
       color: #a8a8b3;
@@ -92,4 +104,10 @@ export const Form = styled.form`
       background: ${shade(0.2, '#84d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
